7e79dc0708ed53833f823db306675cfe8ea45294
jh8579 <jh8579@gmail.com>
2018-03-21 23:31:46 +0900

    solve 1 issue

--- 2018/1260_dfs_bfs/issue/dfs_bfs.cpp
+++ 2018/1260_dfs_bfs/issue/dfs_bfs.cpp
@@ -1,63 +1,73 @@
 // BTS, DTS 문제
+//
 // issue : 메모리 초과
-// 해결방법 : 
+// 원인 : BFS 함수 오류
+// 해결방법 : BFS 함수에 큐를 사용한다.
+//
+// issue :  배열 초과
+// 원인 : 배열 인덱스 딱 맞게 설정
+// 해결방법 : 배열을 원래 조건보다 여유롭게 설정
 
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <queue>
+
 using namespace std;
 
-vector<int> vertex[1000];
+vector<int> vertex[1001];
+queue<int> vertexQueue;
 int numberVertex, numberEdge, startVertex;
-int top, bottom;
-int checkD[1000], checkB[1000];
+int checkD[1001], checkB[1001];
 
-void BTS(int start);
-void DTS(int start);
+void BFS(int start);
+void DFS(int start);
 
 int main() {
 	cin >> numberVertex >> numberEdge >> startVertex;
 
 	for (int i = 0; i < numberEdge; i++) {
+		int top, bottom;
 		cin >> top >> bottom;
 		vertex[top].push_back(bottom);
+		vertex[bottom].push_back(top);	// 양쪽에 모두 추가
 	}
 
 	for (int i = 0; i < numberVertex; i++) {
 		sort(vertex[i].begin(), vertex[i].end());
 	}
 
-	DTS(startVertex);
+	DFS(startVertex);
 	cout << "\n";
-	BTS(startVertex);
+	BFS(startVertex);
 
 	return 0;
 }
 
-void BTS(int start) {
-	if (checkB[start] != 1) {
-		checkB[start] = 1;
-		cout << start << " ";
-	}
-
-	for (int i = 0; i < vertex[start].size(); i++) {
-		if (checkB[vertex[start][i]] != 1) {
-			checkB[vertex[start][i]] = 1;
-			cout << vertex[start][i] << " ";
+void BFS(int start) {
+	vertexQueue.push(start);
+	checkB[start] = 1;
+	int i;
+	while (!vertexQueue.empty()) {
+		int frontVertex = vertexQueue.front();
+		for (i = 0; i < vertex[frontVertex].size(); i++) {
+			if (checkB[vertex[frontVertex][i]] != 1) {
+				checkB[vertex[frontVertex][i]] = 1;
+				vertexQueue.push(vertex[frontVertex][i]);
+			}
 		}
+		cout << vertexQueue.front() << " ";
+		vertexQueue.pop();
 	}
-	for (int i = 0; i < vertex[start].size(); i++) {
-		BTS(vertex[start][i]);
-	}
-
 }
-void DTS(int start) {
+void DFS(int start) {
+	int i;
 	if (checkD[start] != 1) {
 		checkD[start] = 1;
 		cout << start << " ";
 
-		for (int i = 0; i < vertex[start].size(); i++) {
-			DTS(vertex[start][i]);
+		for (i = 0; i < vertex[start].size(); i++) {
+			DFS(vertex[start][i]);
 		}
 	}
 }
