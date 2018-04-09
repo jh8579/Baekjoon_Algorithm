352e6e30b11b4ceccf435a5995ed2fe510cac2b8
Jinho Huh <jh8579@gmail.com>
2018-04-09 17:54:17 +0900

    Solve 1865

--- 2018/1865_wormhole/wormhole.cpp
+++ 2018/1865_wormhole/wormhole.cpp
@@ -1,97 +1,89 @@
 // 웜홀
 //
-//issue 1 : 음수 가중치가 존재하기 때문에 큐로 코딩하면 무한으로 루프가 돌 가능성이 존재한다.
+// issue 1 : 음수 가중치가 존재하기 때문에 큐로 코딩하면 무한으로 루프가 돌 가능성이 존재한다.
+// 해결방법 : 다익스트라를 v-1번 돌려 벨만포드 알고리즘을 사용한다.
 //
+#define _CRT_SECURE_NO_WARNINGS
+#define m(x, y) make_pair(x,y)
+#define INF 0x7fffffff
+
 #include <iostream>
 #include <vector>
+#include <cstring>
+#include <cmath>
+#include <stack>
 #include <queue>
+#include <algorithm>
 
 using namespace std;
-
-struct test {
-	int x, y;
-};
 
 int T, N, M, W;
 
 queue<int> q;
-vector<test> road[510];
+vector<pair<int,int>> road[510];
 
 int visit[510];
 
-int BFS();
+int dikstra();
 
 int main() {
-	cin >> T;
+	scanf("%d", &T);
 
-	for (int k = 0; k < T; k++) {			// 각 테스트 케이스
+	while(T--) {			// 각 테스트 케이스
 
-		cin >> N >> M >> W;
+		scanf("%d%d%d", &N, &M, &W);
 
 		for (int i = 0; i < M; i++) {
 			int s, e, t;
-			cin >> s >> e >> t;
-			road[s].push_back( { e,t } );
-			road[e].push_back( { s,t } );
+			scanf("%d%d%d", &s, &e, &t);
+			road[s].push_back(m(e, t));
+			road[e].push_back(m(s, t));
 		}
 		for (int i = 0; i < W; i++) {
 			int s, e, t;
-			cin >> s >> e >> t;
-			road[s].push_back({ e,-t });
+			scanf("%d%d%d", &s, &e, &t);
+			road[s].push_back(m(e, -t));
 		}
 
-		int flag = BFS();
-
-		if (flag) {
-			cout << "YES" << endl;
+		if (dikstra()) {
+			printf("YES\n");
 		}
 		else {
-			cout << "NO" << endl;
+			printf("NO\n");
+		}
+
+		for (int i = 1; i <= N; i++) {
+			road[i].clear();
 		}
 
 	}	////////////////////////////////////// 각 테스트 케이스
-
 	cin >> N;
 }
 
-int BFS() {
-	for (int k = 1; k <= N; k++) {
-		int cnt = 0;
-		for (int j = 1; j <= N; j++) {			// visit -1로 모두 초기화
+int dikstra() {
 
-			visit[j] = 100000;
-		}
+	int flag;
+	fill(visit, visit + N + 1, INF);
+	visit[1] = 0;
 
-		visit[k] = 0;
-		q.push(k);
+	for (int i = 1; i <= N- 1; i++) {
+		flag = false;
 
-		while (!q.empty()) {
+		for (int j = 1; j <= N; j++) {
 
-			int temp = q.front();
-			q.pop();
-
-			if (temp == k && visit[k] < 0) {
-				return 1;
-			}
-			else if (temp == k && cnt) {
+			if (visit[j] == INF) {
 				continue;
 			}
-			cnt++;
+			for (int k = 0; k < road[j].size(); k++) {
 
-			for (int i = 0; i < road[temp].size(); i++) {
-				if (visit[road[temp][i].x] < 0) {						// 거치지 않은 경우
-					visit[road[temp][i].x] = visit[temp] + road[temp][i].y;
-					q.push(road[temp][i].x);
-				}
-				else {													// 거친 경우
-					if (visit[road[temp][i].x] > visit[temp] + road[temp][i].y) {	// 더 작은 값인 경우 갱신
-						visit[road[temp][i].x] = visit[temp] + road[temp][i].y;
-						q.push(road[temp][i].x);
-					}
+				pair<int, int> next = road[j][k];
+				if (visit[next.first] > visit[j] + next.second) {
+					visit[next.first] = visit[j] + next.second;
+					flag = true;
 				}
 			}
 		}
+		if (!flag) break;
 	}
-	
-	return 0;
+	return flag;
 }
